06f1466709613e996ba2709c4886d992a922ff44
HelenDruzhinskaya <alena1806@mail.ru>
2020-07-22 18:54:33 +0500

    AfterWebinar_version

--- app/src/main/java/ru/samsung/itacademy/mdev/viewpagers/MainActivity.java
+++ app/src/main/java/ru/samsung/itacademy/mdev/viewpagers/MainActivity.java
@@ -1,6 +1,8 @@
 package ru.samsung.itacademy.mdev.viewpagers;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
@@ -9,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.root);
+
+
     }
 }
